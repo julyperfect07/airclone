056fdb04f5737114ec501fac25d6e738f730d5e2
julyperfect07 <abdallah.july05@gmail.com>
2024-03-16 23:59:31 +0300

    header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
-import { BrowserRouter, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Header from './components/Header'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
-        
+        <Route path='/' element={<Home />}></Route>
       </Routes>
     </BrowserRouter>
   )
