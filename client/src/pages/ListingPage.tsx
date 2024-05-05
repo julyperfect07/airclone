f88c0a8b0f9eef725b65307de5ac97a165761ec4
julyperfect07 <abdallah.july05@gmail.com>
2024-05-05 11:24:48 +0300

    creating listing page

--- client/src/pages/ListingPage.tsx
+++ client/src/pages/ListingPage.tsx
@@ -1,9 +1,78 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+interface Listing {
+  title: string;
+  location: string;
+  images: string[];
+  guests: number;
+  rooms: number;
+  bathrooms: number;
+  description: string;
+  category: string;
+}
 
 const ListingPage = () => {
+  const params = useParams();
+  const [listing, setListing] = useState<Listing | null>(null);
+  console.log(listing);
+  useEffect(() => {
+    const getListing = async () => {
+      try {
+        const res = await fetch(
+          `/api/listing/onelisting/${params.id}`
+        );
+        const data = await res.json();
+        if (res.ok) {
+          setListing(data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getListing();
+  }, [params.id]);
+
+  if (!listing) return <div>Loading...</div>;
   return (
-    <div className=" max-w-4xl m-auto mt-7">
-      <h1>name</h1>
+    <div className=" max-w-6xl m-auto mt-7">
+      <h1 className=" font-bold text-lg">{listing.title}</h1>
+      <h2 className=" text-[#767676] mt-3"> {listing.location}</h2>
+      {listing.images && listing.images.length > 0 && (
+        <img
+          src={listing.images[0]}
+          className="w-full object-cover max-h-[700px] rounded-lg mt-3"
+          alt="Listing"
+        />
+      )}
+      <div className=" flex gap-2 w-full mt-5">
+        <div>
+          <h1 className=" font-semibold text-lg">Hosted by </h1>
+          <div className=" flex gap-3 mt-3 ">
+            <span className=" text-[#767676] text-base">
+              {listing.guests} guests
+            </span>
+            <span className=" text-[#767676]">
+              {listing.rooms} rooms
+            </span>
+            <span className=" text-[#767676]">
+              {listing.bathrooms} bathrooms
+            </span>
+          </div>
+          <div className=" flex flex-col mt-5 pt-6 pb-6 border-t-2 border-b-2">
+            <h1 className=" font-semibold text-lg capitalize">
+              {listing.category}
+            </h1>
+            <h2 className=" text-[#767676] ">
+              This property is brand new and luxurious!
+            </h2>
+          </div>
+          <p className=" mt-5 text-lg text-[#767676]">
+            {listing.description}
+          </p>
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
